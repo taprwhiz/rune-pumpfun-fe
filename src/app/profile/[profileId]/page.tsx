7a705655bcf6b7af4eae9f9976b8aadec1028c09
taprwiz <sienkoalbert930607.as@gmail.com>
2024-07-25 02:43:18 +0900

    hotfix: removed console

--- src/app/profile/[profileId]/page.tsx
+++ src/app/profile/[profileId]/page.tsx
@@ -34,7 +34,6 @@ export default function Profile() {
         ...pfp.userInfo,
         multisigWallet: pfp.multisigWallet,
       });
-      console.log("pfp.runes :>> ", pfp.runes);
       setRunes(pfp.runes);
     } catch (error) {}
   };
